047ccba836dc8c75447c8daf322fa63f9ee18516
Edgar Muentes <edgar.j.muentes@aexp.com>
2020-04-29 17:50:35 -0400

    last classes code changes

---
+++ src/games/duckHunt/GameScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+// a React "function component"
+const GameScreen = (props) => {
+  return (
+    <div className="gameScreen">
+      This is the game screen
+    </div>
+  )
+};
+
+export {
+  GameScreen
+}
